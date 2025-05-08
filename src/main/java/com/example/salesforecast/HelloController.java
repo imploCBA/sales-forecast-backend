eb8af954cf85ac7fca51759b052c8ad32d00705c
imploCBA <implocba@mail.ru>
2025-05-08 16:06:50 +0700

    Fix: ensure executable JAR with main class

--- src/main/java/com/example/salesforecast/HelloController.java
+++ src/main/java/com/example/salesforecast/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String redirectToFrontend() {
-        return "redirect:https://ff9b61a7-81c3-4463-966f-5f5fefda124e-00-161grc9y9hswu.worf.replit.dev/.repl.co";
+        return "redirect:https://ff9b61a7-81c3-4463-966f-5f5fefda124e-00-161grc9y9hswu.worf.replit.dev";
     }
 }
