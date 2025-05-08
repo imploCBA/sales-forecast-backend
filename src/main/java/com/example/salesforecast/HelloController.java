c184623b3caebcd48517d86edf07805b71617d02
imploCBA <implocba@mail.ru>
2025-05-08 16:04:23 +0700

    Fix: ensure executable JAR with main class

--- src/main/java/com/example/salesforecast/HelloController.java
+++ src/main/java/com/example/salesforecast/HelloController.java
@@ -1,12 +1,13 @@
 package com.example.salesforecast;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
+
     @GetMapping("/")
-    public String hello() {
-        return "Backend работает!";
+    public String redirectToFrontend() {
+        return "redirect:https://ff9b61a7-81c3-4463-966f-5f5fefda124e-00-161grc9y9hswu.worf.replit.dev/.repl.co";
     }
 }
